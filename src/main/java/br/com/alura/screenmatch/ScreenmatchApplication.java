00a6c3ee72782e591d5f8e1657c7c76175746fdc
magdielrocha <magdiel002@gmail.com>
2025-06-04 11:29:21 -0300

    adiciona a classe Principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,16 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var ConsumoApi = new ConsumoApi();
-		var json = ConsumoApi.obterDados("https://www.omdbapi.com/?t=The+Big+Bang+Theory&apikey=7ed534ca");
-		System.out.println(json);
-//		json = ConsumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dadosFormatados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosFormatados);
-
-
+		Principal principal = new Principal();
+		principal.exbibeMenu();
 	}
 }
